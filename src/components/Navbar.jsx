c7a504f4eb1048f30053dc0cced5e010c06eb083
gitspecific707 <gitspecific707@gmail.com>
2026-01-08 15:11:55 +0530

    default welcome msg updated and logo also

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,17 @@
 
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography, Box } from '@mui/material';
+import shreeram from '../assets/shreeram.avif';
 
 export default function Navbar() {
   return (
     <AppBar position="sticky">
       <Toolbar>
+        <Box
+          component="img"
+          src={shreeram}
+          alt="ShreeRam Printers"
+          sx={{ height: 40, mr: 2 }}
+        />
         <Typography variant="h6" fontWeight="bold">
           ShreeRam Printers
         </Typography>
